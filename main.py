fd92523a72ca6fa8a5a809ae7e80f4f4b7db0004
dragonMau <mau.drogon@gmail.com>
2022-06-28 10:36:34 +0300

    wdw

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from kivy.app import App
-from kivy.uix.label import Label
+import random
 
+import kivy
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+kivy.require('1.9.0')
+
+class MyRoot(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+    
+    def generate_number(self):
+        self.random_label.text = str(random.randint(0, 1000))
 
 class Main(App):
     def build(self):
-        return Label(text='Hello World')
+        return MyRoot()
 
 
 Main().run()
